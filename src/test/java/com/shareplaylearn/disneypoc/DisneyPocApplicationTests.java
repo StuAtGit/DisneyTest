e4b866013e297e06247595afb9d6d8c6a7aeff99
stu s <stu26code@gmail.com>
2019-01-15 11:35:19 -0800

    Initial project skeleton, compiles. Assumes working maven & java installation.

---
+++ src/test/java/com/shareplaylearn/disneypoc/DisneyPocApplicationTests.java
@@ -0,0 +1,17 @@
+package com.shareplaylearn.disneypoc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DisneyPocApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
